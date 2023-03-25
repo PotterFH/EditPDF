bfcec82833f5924924700e7b1403dd49e760b7e6
PotterFH <ivan_2333@outlook.com>
2023-03-24 22:33:04 -0600

    FuncionBack

--- index.php
+++ index.php
@@ -30,9 +30,36 @@ $pdf->SetFont('Helvetica');
 
 // First box - the user's Name
 $pdf->SetFontSize('15'); // set font size
-$pdf->SetXY(150, 10); // set the position of the box
+$pdf->SetXY(150, 5); // set the position of the box
 $pdf->Cell(0, 10, 'Poliza No. ' . $noPoliza , 1, 0, 'C' ); // add the text, align to Center of cell
 
 // render PDF to browser
 $new_pdf = $pdf->Output();
 
+
+ // Create new Landscape PDF
+ $pdf = new \setasign\Fpdi\Fpdi();
+
+ // Reference the PDF you want to use (use relative path)
+ $pagecount = $pdf->setSourceFile('$mes . $pdfname');
+
+ // Import the first page from the PDF and add to dynamic PDF
+ $tpl = $pdf->importPage(1);
+ $pdf->AddPage();
+
+ // Use the imported page as the template
+ $pdf->useTemplate($tpl);
+
+ // Set the default font to use
+ $pdf->SetFont('Helvetica');
+
+ // adding a Cell using:
+ // $pdf->Cell( $width, $height, $text, $border, $fill, $align);
+
+ // Cell with generated text 
+ $pdf->SetFontSize('15'); // set font size
+ $pdf->SetXY(150, 10); // set the position of the box
+ $pdf->Cell(0, 10, 'Poliza No: ' . $noPoliza, 1, 0, 'C'); // add the text, align to Center of cell
+
+ // render PDF to browser
+ $new_pdf = $pdf->Output();
